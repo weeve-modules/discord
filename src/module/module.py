092f731fb24a7d1cbd60655de2ed87d2807107f6
kubagrzelak <jakubgrzelak1@gmail.com>
2022-09-27 20:22:03 +0100

    Implemented module

--- src/module/module.py
+++ src/module/module.py
@@ -6,9 +6,14 @@ Edit this file to implement your module.
 """
 
 from logging import getLogger
+from discordwebhook import Discord
+import os
 
 log = getLogger("module")
 
+log.debug(f'Connecting to Discord client... Webhook URL: {os.getenv("DISCORD_WEBHOOK_URL")}')
+client = Discord(url=os.getenv("DISCORD_WEBHOOK_URL"))
+log.debug('Successfully connected to Discord client!')
 
 def module_main(received_data: any) -> str:
     """
@@ -23,10 +28,14 @@ def module_main(received_data: any) -> str:
 
     """
 
-    log.debug("Outputting ...")
+    log.debug("Sending data to Discord channel ...")
 
     try:
         # YOUR CODE HERE
+        resp = client.post(content=received_data[os.getenv("MESSAGE_LABEL")])
+
+        if resp.status_code != 204:
+            return f"Error when sending data to Discord channel. Server response: {resp.status_code}, {resp.text}"
 
         return None
 
